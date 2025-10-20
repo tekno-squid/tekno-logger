d4175ad8877d81cf4a605ec9ca5c8c3a6f7784a6
tekno_squid <the.tekno.squid@gmail.com>
2025-10-20 12:24:22 -0400

    fix: remove obsolete ALTER TABLE statement from 002_maintenance_state.sql

--- migrations/002_maintenance_state.sql
+++ migrations/002_maintenance_state.sql
@@ -12,6 +12,3 @@ CREATE TABLE IF NOT EXISTS maintenance_state (
 -- Insert initial maintenance state record
 INSERT IGNORE INTO maintenance_state (last_maintenance, maintenance_in_progress) 
 VALUES (CURRENT_TIMESTAMP, 0);
-
--- Rename fingerprints table to fingerprint_trackers for consistency
-ALTER TABLE fingerprints RENAME TO fingerprint_trackers;
